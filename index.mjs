e36764534d764a254ccac87d87f65bb221dce337
Petr Tripolsky <tripolskypetr@gmail.com>
2024-11-08 11:28:01 +0500

    init-method

--- index.mjs
+++ index.mjs
@@ -1,10 +1,23 @@
 export const createActivator = (namespace = "unknown") => {
 
+    const ACTIVATOR_FN = Symbol('activator-fn');
     const INIT_FN = Symbol('init-fn');
 
     const accessorMap = new Map();
     const factoryMap = new Map();
     const instanceMap = new Map();
+
+    const singleshot = (run) => {
+        let isInitComplete = false;
+        let runValue = undefined;
+        return () => {
+            if (isInitComplete) {
+                return runValue;
+            }
+            isInitComplete = true;
+            return runValue = run();
+        }
+    };
 
     const getInstance = (name) => {
         if (instanceMap.has(name)) {
@@ -19,14 +32,21 @@ export const createActivator = (namespace = "unknown") => {
         return {};
     }
     
-    const createInitializer = (name, self) => () => {
-        Object.setPrototypeOf(self, getInstance(name));
-    };
-    
+    const createActivator = (name, self) => 
+        singleshot(() => {
+            Object.setPrototypeOf(self, getInstance(name));
+        });
+
+    const createInitializer = (self) => 
+        singleshot(() => {
+            self.init && self.init();
+        });
+
     class InstanceAccessor {
         constructor(name) {
             this.name = name;
-            this[INIT_FN] = createInitializer(name, this);
+            this[ACTIVATOR_FN] = createActivator(name, this);
+            this[INIT_FN] = createInitializer(this);
         }
     }
     
@@ -43,6 +63,9 @@ export const createActivator = (namespace = "unknown") => {
         : accessorMap.set(name, new InstanceAccessor(name)).get(name);
     
     const init = () => {
+        for (const accessor of accessorMap.values()) {
+            accessor[ACTIVATOR_FN]();
+        }
         for (const accessor of accessorMap.values()) {
             accessor[INIT_FN]();
         }
