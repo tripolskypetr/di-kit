8761d274e3298acc1a532216ef0383edf145884b
Petr Tripolsky <tripolskypetr@gmail.com>
2025-06-15 18:28:27 +0500

    patch

--- index.d.ts
+++ index.d.ts
@@ -4,13 +4,13 @@ declare module "di-kit" {
     export function provide<T = object>(key: Key, ctor: T | (() => T)): void
     export function init(): void;
     export function override<T = object>(key: Key, instance: T): void
-    export function beforeInit(name: string, override: <T = object>(key: Key, instance: T) => void);
+    export function beforeInit(fn: (name: string, override: <T = object>(key: Key, instance: T) => void) => void): () => void;
     export function createActivator(name?: string): {
         inject<T = object>(key: Key): T;
         provide<T = object>(key: Key, ctor: T | (() => T)): void
         init(): void;
         override<T = object>(key: Key, instance: T): void
-        beforeInit(name: string, override: <T = object>(key: Key, instance: T) => void);
+        beforeInit(fn: (name: string, override: <T = object>(key: Key, instance: T) => void) => void): () => void;
         InstanceAccessor: InstanceAccessor;
     };
     export class InstanceAccessor {
